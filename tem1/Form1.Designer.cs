514b5bebe4e0d1cc671c8a02b4cfdf8a468b2b1e
ddarling-ddarkling <ddarling.ddarkling@gmail.com>
2017-03-10 00:09:30 +0300

    final
    Вычисление синуса аргумента, вводимого пользователем

---
+++ tem1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace tem1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_X = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_result = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::tem1.Properties.Resources.Безымянный;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(555, 82);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите точность: 0 < a < 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(309, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите аргумент X:";
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(26, 138);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(100, 20);
+            this.textBox_a.TabIndex = 3;
+            this.textBox_a.TextChanged += new System.EventHandler(this.textBox_a_TextChanged);
+            this.textBox_a.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_a_KeyPress);
+            // 
+            // textBox_X
+            // 
+            this.textBox_X.Location = new System.Drawing.Point(312, 138);
+            this.textBox_X.Name = "textBox_X";
+            this.textBox_X.Size = new System.Drawing.Size(100, 20);
+            this.textBox_X.TabIndex = 4;
+            this.textBox_X.TextChanged += new System.EventHandler(this.textBox_a_TextChanged);
+            this.textBox_X.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_a_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(29, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ВЫЧИСЛИТЬ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_result
+            // 
+            this.label_result.BackColor = System.Drawing.Color.White;
+            this.label_result.Location = new System.Drawing.Point(26, 233);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(555, 95);
+            this.label_result.TabIndex = 6;
+            this.label_result.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 337);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_X);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Вычисление функции с помощью разложения в ряд";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_X;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_result;
+    }
+}
+
---
+++ tem1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tem1
+{
+    public partial class Form1 : Form
+    {
+        char decimalSeparator = Convert.ToChar(System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.CurrencyDecimalSeparator);
+
+        public static double Factorial(int N) 
+        {
+            double fact = 1;
+            for (int i = 1; i <= N; i++)
+                fact *= i;
+            return fact;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox_X.Text);                // аргумент функции
+            double a = Convert.ToDouble(textBox_a.Text);                // точность
+            double sin, element, raz, summ = 1, previous = 1;           // sin - функция, summ - сумма ряда, raz - разность между соседними членами ряда, element - эл-т ряда, previous - предыдущий член ряда
+            int n = 1;                                                  // n - счётчик
+
+            if ( 0 < a && a < 1)
+            {
+                do
+                {
+                    element = ((Math.Pow(-1, n) * Math.Pow(x, 2 * n)) / Factorial(2 * n + 1));
+                    summ += element;
+                    raz = element - previous;
+                    previous = element;
+                    n++;
+                }
+                while (Math.Abs(raz) > a);
+
+                sin = (x * summ);
+
+                label_result.Text = "Результат:\n" +
+                                    "Sin(x) = " + Convert.ToString(sin) +
+                                    "\nСумма ряда = " + Convert.ToString(summ) +
+                                    "\nКоличество членов ряда = " + Convert.ToString(n) +
+                                    "\nМат.Син. = " + Convert.ToString(Math.Sin(x));
+            }
+            else
+            {
+                MessageBox.Show("Значение точности не соответствует условию!");
+            }
+        }
+
+        private void textBox_a_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || e.KeyChar == decimalSeparator || e.KeyChar == (Char)Keys.Delete || e.KeyChar == (Char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_a_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox_a.Text.Length == 0 || textBox_X.Text.Length == 0) button1.Enabled = false;
+                else button1.Enabled = true;
+        }
+
+        private void textBox_X_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
